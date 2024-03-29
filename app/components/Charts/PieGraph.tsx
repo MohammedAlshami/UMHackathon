cace3110ae5a52528ede54079981549e17b6a029
Dahlia <nuradzainala@gmail.com>
2024-03-29 17:10:17 +0800

    Frontend 2/3

--- app/components/Charts/PieGraph.tsx
+++ app/components/Charts/PieGraph.tsx
@@ -7,13 +7,16 @@ export default function PieGraph() {
     series={[
       {
         data: [
-          { id: 0, value: 10, label: 'series A' },
-          { id: 1, value: 15, label: 'series B' },
-          { id: 2, value: 20, label: 'series C' },
+          { id: 0, value: 10, label: 'Medical' },
+          { id: 1, value: 20, label: 'Trasnportation' },
+          { id: 2, value: 35, label: 'Food & Beverages' },
+          { id: 3, value: 5, label: 'Entertainment' },
+          { id: 4, value: 20, label: 'Education' },
+          { id: 5, value: 10, label: 'Services' },
         ],
       },
     ]}
-    width={400}
+    width={500}
     height={200}
   />
   );
